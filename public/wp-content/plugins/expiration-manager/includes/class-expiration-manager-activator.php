d53d00946fdde91dcce4677fcc8e8fc46716253d
Alessandro Giuzio <38163055+alessandro-giuzio@users.noreply.github.com>
2025-12-18 16:42:31 +0100

    Add Expiration Manager plugin with initial implementation

---
+++ public/wp-content/plugins/expiration-manager/includes/class-expiration-manager-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://example.com
+ * @since      1.0.0
+ *
+ * @package    Expiration_Manager
+ * @subpackage Expiration_Manager/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Expiration_Manager
+ * @subpackage Expiration_Manager/includes
+ * @author     Alessandro <test@mail.com>
+ */
+class Expiration_Manager_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/wp-content/plugins/expiration-manager/includes/class-expiration-manager-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://example.com
+ * @since      1.0.0
+ *
+ * @package    Expiration_Manager
+ * @subpackage Expiration_Manager/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Expiration_Manager
+ * @subpackage Expiration_Manager/includes
+ * @author     Alessandro <test@mail.com>
+ */
+class Expiration_Manager_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
